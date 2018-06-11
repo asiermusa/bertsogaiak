4bb88d3b815f51a8669ab0cb9f33f1067cc97c42
asiermusa <asiermusa@gmail.com>
2018-06-11 13:47:36 +0200

    Initial release

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const config = {
+  apiKey: "AIzaSyCQxB7IrvsQzkAbqoT2iMTFdCCJwBeLk5A",
+  authDomain: "bertso-gaiak.firebaseapp.com",
+  databaseURL: "https://bertso-gaiak.firebaseio.com",
+  projectId: "bertso-gaiak",
+  storageBucket: "bertso-gaiak.appspot.com",
+  messagingSenderId: "61412632326"
+};
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import {store} from './store'
+import {routes} from './routes'
+import App from './App.vue'
+import Header from './components/Header.vue'
+import firebase from 'firebase'
+import {config} from './config'
+
+//firebase
+firebase.initializeApp(config);
+
+//vue router
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
+})
+router.beforeEach((to, from, next) => {
+  let currentUser = 4
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth && !currentUser) {
+    document.title = (to.meta.title ? to.meta.title + ' - ' : '') + 'Bertso Gaiak';
+    next('/')
+  } else {
+    document.title = (to.meta.title ? to.meta.title + ' - ' : '') + 'Bertso Gaiak';
+    next()
+  }
+})
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+  created() {
+    this.$store.dispatch('a_authStateObserver')
+  }
+})
